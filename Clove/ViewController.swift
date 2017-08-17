50c6b66bc1283edc7dc61f681d9b6e79206f77a8
Sivaprakash Ragavan <mail@sivragav.com>
2017-08-17 00:07:48 -0400

    Initial commit - Generated in Xcode v8.3.3

---
+++ Clove/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Clove
+//
+//  Created by Sivaprakash Ragavan on 8/17/17.
+//  Copyright © 2017 Clove HQ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
