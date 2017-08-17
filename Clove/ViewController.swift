12bac7a60d9f3a8524934b798851a6398cf9162f
Sivaprakash Ragavan <mail@sivragav.com>
2017-08-17 07:47:14 -0400

    Added recording options

--- Clove/ViewController.swift
+++ Clove/ViewController.swift
@@ -7,19 +7,128 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVAudioRecorderDelegate {
+    
+    //Outlets
+    @IBOutlet weak var recordingTimeLabel: UILabel!
+    
+    //Variables
+    var audioRecorder: AVAudioRecorder!
+    var meterTimer:Timer!
+    var isAudioRecordingGranted: Bool!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        switch AVAudioSession.sharedInstance().recordPermission() {
+        case AVAudioSessionRecordPermission.granted:
+            isAudioRecordingGranted = true
+            break
+        case AVAudioSessionRecordPermission.denied:
+            isAudioRecordingGranted = false
+            break
+        case AVAudioSessionRecordPermission.undetermined:
+            AVAudioSession.sharedInstance().requestRecordPermission() { [unowned self] allowed in
+                DispatchQueue.main.async {
+                    if allowed {
+                        self.isAudioRecordingGranted = true
+                    } else {
+                        self.isAudioRecordingGranted = false
+                    }
+                }
+            }
+            break
+        default:
+            break
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        audioRecorder = nil
     }
-
-
+    
+    //MARK:- Audio recorder buttons action.
+    @IBAction func audioRecorderAction(_ sender: UIButton) {
+        
+        if isAudioRecordingGranted {
+            
+            //Create the session.
+            let session = AVAudioSession.sharedInstance()
+            
+            do {
+                //Configure the session for recording and playback.
+                try session.setCategory(AVAudioSessionCategoryPlayAndRecord, with: .defaultToSpeaker)
+                try session.setActive(true)
+                //Set up a high-quality recording session.
+                let settings = [
+                    AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+                    AVSampleRateKey: 44100,
+                    AVNumberOfChannelsKey: 2,
+                    AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+                ]
+                //Create audio file name URL
+                let audioFilename = getDocumentsDirectory().appendingPathComponent("audioRecording.m4a")
+                //Create the audio recording, and assign ourselves as the delegate
+                audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
+                audioRecorder.delegate = self
+                audioRecorder.isMeteringEnabled = true
+                audioRecorder.record()
+                meterTimer = Timer.scheduledTimer(timeInterval: 0.1, target:self, selector:#selector(self.updateAudioMeter(timer:)), userInfo:nil, repeats:true)
+            }
+            catch let error {
+                print("Error for start audio recording: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    @IBAction func stopAudioRecordingAction(_ sender: UIButton) {
+        
+        finishAudioRecording(success: true)
+        
+    }
+    
+    func finishAudioRecording(success: Bool) {
+        
+        audioRecorder.stop()
+        audioRecorder = nil
+        meterTimer.invalidate()
+                
+        if success {
+            print("Recording finished successfully.")
+        } else {
+            print("Recording failed :(")
+        }
+    }
+    
+    func updateAudioMeter(timer: Timer) {
+        
+        if audioRecorder.isRecording {
+            let hr = Int((audioRecorder.currentTime / 60) / 60)
+            let min = Int(audioRecorder.currentTime / 60)
+            let sec = Int(audioRecorder.currentTime.truncatingRemainder(dividingBy: 60))
+            let totalTimeString = String(format: "%02d:%02d:%02d", hr, min, sec)
+            recordingTimeLabel.text = totalTimeString
+            audioRecorder.updateMeters()
+        }
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+    }
+    
+    //MARK:- Audio recoder delegate methods
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        
+        if !flag {
+            finishAudioRecording(success: false)
+        }
+    }
 }
-
